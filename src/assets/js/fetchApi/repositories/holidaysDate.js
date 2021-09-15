9356d1b862b38b456064440bc11437e1d5b64ad8
daisuke-tanabe <helloworld0840@gmail.com>
2021-09-15 14:52:36 +0900

    chore(npm): building a development environment for Vue3 and typescript

--- src/assets/js/fetchApi/repositories/holidaysDate.js
+++
@@ -1,9 +0,0 @@
-// 共通のaxiosインスタンス
-import client from '../client/holidays';
-
-// リソースの取得先
-const resources = '/date.json';
-
-export default {
-  get: () => client.get(resources),
-};
--- src/utilities/zeroPadding.js
+++
@@ -1,3 +0,0 @@
-export default function addZeroPadding(num, length = 2) {
-  return `${num}`.padStart(length, '0');
-}
